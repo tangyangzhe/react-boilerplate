6836f183d0bcae5f7f9250632eec70b4a67f2b97
tangyz <tangyangzhe@126.com>
2017-08-15 19:59:08 +0800

    set NODE_ENV use corss-env, add DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,10 @@ const config = {
       hash: true
     }),
     new ExtractTextPlugin('css/[name].bundle.css'),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ],
 
   // When importing a module whose path matches one of the following, just
